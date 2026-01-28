da4cb0fe0fc877dd43af9f7ff798ee430ea2e0d8
artrctx <artrctx@gmail.com>
2026-01-27 20:21:09 -0600

    playing with endian

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 new:
 	mkdir $(name) && cd ./$(name) && go mod init $(name) && touch main.go; echo "package main" >> main.go
+play:
+	go run ./playground/main.go
--- playground/main.go
+++ playground/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 )
 
-func test(_ []int, _ int) int {
-	return -1
-}
-
-func max(nums []int) (int, int) {
-	idx, max := 0, nums[0]
-	for i_idx, v := range nums {
-		if max > v {
-			continue
-		}
-		idx = i_idx
-		max = v
-	}
-	return idx, max
-}
-
 func main() {
-	fmt.Println(test([]int{1, 2, 3}, 1))
+	val := make([]byte, 8)
+	binary.BigEndian.PutUint64(val, 0x4f52)
+	fmt.Println(val)
+	fmt.Printf("-- %d %d\n", 0x4f, 0x52)
 }
