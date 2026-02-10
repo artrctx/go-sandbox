ad05cfc9cba2447bddb974fc047652efc6363128
artrctx <artrctx@gmail.com>
2026-02-09 18:43:45 -0600

    working on scraper why not

--- scraper/go.mod
+++ scraper/go.mod
@@ -4,4 +4,4 @@ go 1.25.6
 
 require github.com/twpayne/go-kmz v0.0.0-20160614194227-165281381e72
 
-require github.com/twpayne/go-kml v1.5.2 // indirect
+require github.com/twpayne/go-kml v1.5.2
--- scraper/main.go
+++ scraper/main.go
@@ -12,14 +12,30 @@ import (
 // Name | Address | Business Type | Description (if any) | Website
 // https://apps.ilsos.gov/businessentitysearch/businessentitysearch
 // https://www.google.com/maps/d/u/0/viewer?mid=18EFqHj_e5KFy_iknwP8aGpMp7JQGDyc&ll=41.96780775781806%2C-87.71260880628354&z=16
+// https://developers.google.com/kml/documentation/kmlreference
 func main() {
-	kmz := kmz.NewKMZ(kml.Placemark(
-		kml.Name("Simple placemark"),
-		kml.Description("Attached to the ground. Intelligently places itself at the height of the underlying terrain."),
-		kml.Point(
-			kml.Coordinates(kml.Coordinate{Lon: -122.0822035425683, Lat: 37.42228990140251}),
+	kmz := kmz.NewKMZ(
+		kml.Folder(
+			kml.Name("this is folder"),
+			kml.Placemark(
+				kml.Name("Simple placemark"),
+				kml.Description("Attached to the ground. Intelligently places itself at the height of the underlying terrain."),
+				kml.Point(
+					kml.Coordinates(kml.Coordinate{Lon: -122.0822035425683, Lat: 37.42228990140251}),
+				),
+			),
 		),
-	))
+		kml.Folder(
+			kml.Name("this is folder2"),
+			kml.Placemark(
+				kml.Name("Simple placemark"),
+				kml.Description("Attached to the ground. Intelligently places itself at the height of the underlying terrain."),
+				kml.Point(
+					kml.Coordinates(kml.Coordinate{Lon: -122.0822035425683, Lat: 37.42228990140251}),
+				),
+			),
+		),
+	)
 
 	w := &bytes.Buffer{}
 	if err := kmz.WriteIndent(w, "", "\t"); err != nil {
