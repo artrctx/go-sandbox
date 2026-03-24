0368a39e4c475f4bc204fde2a0e46d75cbb69387
artrctx <artrctx@gmail.com>
2026-03-23 22:08:01 -0500

    trying out traning here

---
+++ num_recog/go.mod
@@ -0,0 +1,3 @@
+module num_recog
+
+go 1.26.1
---
+++ num_recog/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// https://medium.com/@Kiana-Jafari/digit-recognition-building-a-deep-neural-network-from-scratch-132bd574564a
+func main() {
+	fmt.Println("num recog")
+}
