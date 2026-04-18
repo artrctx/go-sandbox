c8e236f6053e68a78ee2ba55aa2f36643cbe3dd2
artrctx <artrctx@gmail.com>
2026-04-17 21:23:12 -0500

    working on finetune example

--- finetune/go.mod
+++ finetune/go.mod
@@ -1,3 +1,8 @@
 module finetune
 
 go 1.26.2
+
+require (
+	github.com/go-logr/logr v1.4.1 // indirect
+	k8s.io/klog/v2 v2.140.0 // indirect
+)
--- finetune/main.go
+++ finetune/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"k8s.io/klog/v2"
+)
 
 // https://go.dev/wiki/AI
 // https://github.com/gomlx/gomlx
 // https://arxiv.org/abs/2502.01225
+// https://github.com/gomlx/gomlx/tree/main/examples/dogsvscats
 func main() {
-	fmt.Println("Fine tune test")
+	klog.InitFlags(nil)
+	klog.Infoln("THIS IS INFO")
+	klog.Errorln("THIS IS ERROR")
 }
